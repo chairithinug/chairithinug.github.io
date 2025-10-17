78125502225037630cb12359f905c70141499c15
Anapat Chairithinugull <chairithinug@uwalumni.com>
2025-10-17 22:43:33 +0200

    articles

--- articles/post-footer.js
+++ articles/post-footer.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('/articles/post-footer.html')
         .then(res => res.text())
         .then(html => {
-            document.getElementById('post-footer-container').innerHTML = html;
-            setupCookies();
+            document.getElementById('post-footer-container').innerHTML = html
         });
 });
